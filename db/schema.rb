80fd002674a2075094e384fdb51096de9320b783
keitoyokoi0628 <d51makbbcf@gmail.com>
2022-06-12 13:57:38 +0000

    マイグレーションとdb:seed完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_11_115651) do
+ActiveRecord::Schema.define(version: 2022_06_12_134316) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -38,6 +38,16 @@ ActiveRecord::Schema.define(version: 2022_06_11_115651) do
     t.integer "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "comics", force: :cascade do |t|
+    t.string "title"
+    t.integer "serialization_started_year"
+    t.integer "serialization_end_year"
+    t.string "publisher"
+    t.string "genre"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
