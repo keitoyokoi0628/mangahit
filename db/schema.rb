3abf3b654c3405b74ab36a7174c23b4fe8a71277
keitoyokoi0628 <d51makbbcf@gmail.com>
2022-06-12 14:19:30 +0000

    Game　モデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_12_134316) do
+ActiveRecord::Schema.define(version: 2022_06_12_141752) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -46,6 +46,13 @@ ActiveRecord::Schema.define(version: 2022_06_12_134316) do
     t.integer "serialization_end_year"
     t.string "publisher"
     t.string "genre"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "games", force: :cascade do |t|
+    t.string "status"
+    t.string "result"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
