66181174904ee4577c249ac259144d87c977058b
keitoyokoi0628 <d51makbbcf@gmail.com>
2022-06-13 11:19:44 +0000

    route記載

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'games#new'
-  resources :games
+  resources :games, only: %i[new create] do
+    resources :progresses, only: %i[new create]
+  end
 end
