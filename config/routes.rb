16d143b9c8acb79eb5918234882e0383f2304867
keitoyokoi0628 <d51makbbcf@gmail.com>
2022-06-14 12:32:27 +0000

    ルーティングにgive_upを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'games#new'
-  
+
   resources :games, only: %i[new create] do
+
+    member do
+      get :give_up
+    end
+
     resources :progresses, only: %i[new create]
   end
-  
+
 end
