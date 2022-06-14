1b6c5ee6270018c840638c40d87c4c704928c633
keitoyokoi0628 <d51makbbcf@gmail.com>
2022-06-14 12:48:43 +0000

    progressモデルに分岐条件を記載

--- app/models/progress.rb
+++ app/models/progress.rb
@@ -14,4 +14,12 @@ class Progress < ApplicationRecord
     self.sequence = next_sequence
   end
 
+  def positive_answer?
+    answer == 'positive'
+  end
+
+  def negative_answer?
+    answer == 'negative'
+  end
+
 end
