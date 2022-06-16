99485f5478960c4baef8ae807eaa13b1ab84dc83
keitoyokoi0628 <d51makbbcf@gmail.com>
2022-06-16 12:32:21 +0000

    ExtractionAlgorithm.rbの誤記修正とprogressコントローラーの誤記修正,動作test完了

--- app/algorithms/extraction_algorithm.rb
+++ app/algorithms/extraction_algorithm.rb
@@ -1,6 +1,6 @@
-class Extract ionAlgorithm
-  after_reader :game
-  after_reader :query
+class ExtractionAlgorithm
+  attr_reader :game
+  attr_reader :query
 
   def initialize(game)
     Rails.logger.debug('ExtractionAlgorithm initialized')
@@ -12,11 +12,11 @@ class Extract ionAlgorithm
     progresses = @game.progresses
     progresses.each do |progress|
 
-      question = progress.question
+     question = progress.question
 
 
-      Rails.logger.debug('On the way quary is' + @query.to_sqi.to_s)
-      Rails.logger.debug('On the way comics are' + @quary.pluck(:title).to_a.to_s)
+      Rails.logger.debug('On the way quary is ' + @query.to_sql.to_s)
+      Rails.logger.debug('On the way comics are ' + @query.pluck(:title).to_a.to_s)
     end
     @quary
   end
