3831cb8c5aac99dd3373ef7aae176b3109d0b752
keitoyokoi0628 <d51makbbcf@gmail.com>
2022-06-16 12:18:35 +0000

    アルゴリズムのひな形を作成

---
+++ app/algorithms/extraction_algorithm.rb
@@ -0,0 +1,23 @@
+class Extract ionAlgorithm
+  after_reader :game
+  after_reader :query
+
+  def initiallize(game)
+    Rails.logger.debug('ExtractionAlgorithm initialized')
+    @game = game
+    @query = Comic.all
+  end
+
+  def compute
+    progresses = @game.progresses
+    progresses.each do |progress|
+
+      question = progress.question
+
+
+      Rails.logger.debug('On the way quary is' + @query.to_sqi.to_s)
+      Rails.logger.debug('On the way comics are' + @quary.pluck(:title).to_a.to_s)
+    end
+    @quary
+  end
+end
