348fbdabba85ad632a31cfd6a00b130faa6fd764
keitoyokoi0628 <d51makbbcf@gmail.com>
2022-06-20 04:37:30 +0000

    gameコントローラにupdateアクション、correctアクションを記載

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,6 @@
 class GamesController < ApplicationController
 
   def new
-
   end
 
   def create
@@ -16,7 +15,27 @@ class GamesController < ApplicationController
   end
 
   def give_up
+  end
 
+  def update
+    current_game = Game.find(params[:id])
+
+    if params[:correct].present?
+      current_game.status = 'finished'
+      current_game.result = :correct
+      current_game.save!
+
+      redirect_to correct_game_path(current_game)
+    else
+      current_game.status = 'finished'
+      current_game.result = :incorrect
+      current_game.save!
+
+      redirect_to give_up_game_path(current_game)
+    end
+  end
+
+  def correct
   end
 
 
