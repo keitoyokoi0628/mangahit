228998d20370b89e465ec344cec2a01a2da8c70a
keitoyokoi0628 <d51makbbcf@gmail.com>
2022-06-20 04:31:43 +0000

    ルーティングgameにupdate correctを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'games#new'
 
-  resources :games, only: %i[new create] do
+  resources :games, only: %i[new create update] do
 
     member do
       get :give_up
       get :challenge
+      get :correct
     end
 
     resources :progresses, only: %i[new create]
